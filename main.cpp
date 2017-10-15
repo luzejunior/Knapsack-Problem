3e9d802e97ea307823cba96e2a76a573a0d0ff6c
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2017-10-15 12:15:28 -0300

    First matrix implementation tests done

--- main.cpp
+++ main.cpp
@@ -27,7 +27,9 @@ SOFTWARE.
 */
 
 #include <iostream>
+#include <algorithm>
 #include "src/knapsack.h"
+#include "src/matrix.h"
 
 using namespace std;
 
@@ -39,9 +41,39 @@ int main(void){
   k1.addItemtoVector(new Item(17, 27));
   k1.addItemtoVector(new Item(19, 25));
 
+  sort(k1.itens_vector.begin(), k1.itens_vector.end(), [](const Item* lhs, const Item* rhs){ return lhs->weight < rhs->weight; });
+
   for(vector<Item*>::iterator it = k1.itens_vector.begin(); it != k1.itens_vector.end(); it++){
     cout << "This knapsack iten has weight: " << (*it)->weight << " and value: " << (*it)->value << endl;
   }
+
+  Matrix m1(k1.itens_number, k1.knapsack_max_weight);
+
+  vector<vector<int>> *auxMatrix = m1.matrix;
+  for(int i=0; i<k1.knapsack_max_weight; i++){
+    (*auxMatrix)[0][i] = 0;
+  }
+
+  for(int j=1; j<k1.itens_number+1; j++){
+    for(int i=1; i<k1.knapsack_max_weight+1; i++){
+      int value = m1.checkWeight(i, k1.itens_vector[j-1]);
+      if(value > 0){
+        int nextIndex = i - j;
+        value += (*auxMatrix)[j-1][nextIndex];
+        (*auxMatrix)[j][i] = value;
+      }
+      else (*auxMatrix)[j][i] = (*auxMatrix)[j-1][i];
+    }
+  }
+
+  for(int i=0; i<k1.itens_number+1; i++){
+    for(int j=0; j<k1.knapsack_max_weight+1; j++){
+      cout << "[" << (*auxMatrix)[i][j] << "]";
+    }
+    cout << endl;
+  }
+
+
 
   return 0;
 }
